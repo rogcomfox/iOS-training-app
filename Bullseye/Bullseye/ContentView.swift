deec9b11d1b71e3417e23aceed9479857a042b7c
rogcomfox <joeg.11021996@gmail.com>
2020-09-13 13:59:43 +0700

    init project

---
+++ Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Joe G on 13/09/20.
+//  Copyright © 2020 Nusantarian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
